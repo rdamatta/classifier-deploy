05bbb2390d2dfa3df74d50c9ae8ebbeedb5d6f45
Rodrigo da Matta <rodrigo@damatta.email>
2021-05-15 02:02:44 +0300

    Add files via upload

--- app.py
+++ app.py
@@ -83,18 +83,16 @@ def run():
 
         if st.button("Predict"):
             output = predict(model=model, input_df=input_df)
-        if output == 'A':
-            res = "Oil properties are within acceptable limits and operation can continue as usual."
-        else:
-            res = "Certain results are outside acceptable ranges, minor problems with machinery."
-        #elif output == 'B':
-            #res = "Certain results are outside acceptable ranges, minor problems with machinery."
-        #elif output == 'C':
-            #res = "Unsatisfactory results are present, significant problem with the compartment and lubricant properties."
-        #elif output == 'X':
-            #res = "Clear contamination needing immediate diagnostic and corrective action to prevent possible failure."
+            #if output == 'A':
+                #res = "Oil properties are within acceptable limits and operation can continue as usual."
+            #elif output == 'B':
+                #res = "Certain results are outside acceptable ranges, minor problems with machinery."
+            #elif output == 'C':
+                #res = "Unsatisfactory results are present, significant problem with the compartment and lubricant properties."
+            #elif output == 'X':
+                #res = "Clear contamination needing immediate diagnostic and corrective action to prevent possible failure."
 
-        st.success('The outcome is: {}'.format(res))
+        st.success('The outcome is: {}'.format(output))
 
     if add_selectbox == 'Batch':
 
