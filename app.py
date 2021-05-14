c628f6e4e55a8bff847974fe2c082f536cd15e03
Rodrigo da Matta <rodrigo@damatta.email>
2021-05-14 21:12:52 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import numpy as np
 import pickle
 
-model = load_model('deployment_13052021')
+model = load_model('deployment_14052021')
 
 def predict(model, input_df):
     predictions_df = predict_model(estimator=model, data=input_df)
